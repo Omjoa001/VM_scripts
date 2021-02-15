9b56b27f11653eec873f3c89ec80fb6e7867178b
manager <manager@spelunk.com>
2021-02-15 17:41:50 +0000

    Added most functionality. Needs testing

--- manager/anti_leeshore.sh
+++ manager/anti_leeshore.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
 set -x
 
-# re-enable vms that have been shut down by leeshore
+## Anti-Leeshore
+# Re-enable vms that have been shut down by leeshore
+# WIP
+
+# Check if vms are shut down
+# If so, start the vm
+# + Special case for database (need to start cockroach)
+
+# We should have a list of disabled servers, so that
+# the script doesn't restart things we have shut down on purpose
+# But laziness -> hard coded if for now
+#DISABLED=""
+
+# Servers that are down
+SHUTDOWN=$(openstack server list | grep SHUTOFF | awk '{print $4}')
+
+for SERVER in $SHUTDOWN; do
+    if [ "$SERVER" != "worker1" ] && [ "$SERVER" != "worker2" ]; then
+
+        openstack server start $SERVER
+
+        if [ "$SERVER" -eq "db1" ]; then
+            ../db/start_db.sh
+        fi
+
+        # We can add more special cases here
+    fi
+done
