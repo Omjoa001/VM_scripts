9a71f583ab255ac0ac1866bf110818182e8bd2b8
manager <manager@spelunk.com>
2021-02-15 13:37:35 +0000

    some comments. init line doesn't seem to work

--- db/start_db.sh
+++ db/start_db.sh
@@ -1,9 +1,13 @@
 
 #!/bin/env bash
 
+set -x
+
 DB1=192.168.132.33
 
-# This hasn't been tested
+# This seems to work, but it throws a warning because of --insecure 
 ssh $DB1 sudo cockroach start --insecure --store=/bfdata --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background --join=localhost:26257
-ssh $DB1 sudo cockroach init --insecure --host=localhost:26257
+
+# Not sure if this is needed. The script usually halts when it reaches this line
+# ssh $DB1 sudo cockroach init --insecure --host=localhost:26257
 
