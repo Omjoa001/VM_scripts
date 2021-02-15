d47261f5edc31d1757d1d286857dbc37662f103f
t e <tmaregge@gmail.com>
2021-02-15 14:57:15 +0000

    Update create_db_vm.sh

--- db/create_db_vm.sh
+++ db/create_db_vm.sh
@@ -22,5 +22,6 @@ NEW_IP=$(openstack server list | grep db1 | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1
 OLD_IP=$(grep dbhost ~/bookface/config.php | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 
 # Replace dbhost with new IP in config.php
+# Add -i when done
 sed "s/$dbhost = \"${OLD_IP}\";/$dbhost = \"${NEW_IP}\";/g" ~/bookface/config.php
 
